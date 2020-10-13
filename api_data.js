6792754537a97d3e5d6a6debd8f222170f8a0428
DEF4ULT-OP <DEF4ULT-OP@users.noreply.github.com>
2020-10-13 15:24:23 +0000

    Site build: 995b29f36533e4acfd83eff74f63c1d235cadf14

--- api_data.js
+++ api_data.js
@@ -18,8 +18,121 @@ define({ "api": [
       }
     },
     "version": "0.0.0",
-    "filename": "Routes/User.js",
-    "groupTitle": "User",
-    "name": "GetUserId"
+    "filename": "./Routes/User.js",
+    "groupTitle": "",
+    "name": "GetUserId",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "_id",
+            "description": "<p>Id of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "username",
+            "description": "<p>Name of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Email address of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "verified",
+            "description": "<p>Is email address verified</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": true,
+            "field": "bio",
+            "description": "<p>Bio of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": true,
+            "field": "dob",
+            "description": "<p>Date of birth of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": true,
+            "field": "phoneNumber",
+            "description": "<p>Phone number of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": true,
+            "field": "image",
+            "description": "<p>Profile image url of the user</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "createdAt",
+            "description": "<p>Timestamp of user creation</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "updatedAt",
+            "description": "<p>Timestamp of user updation</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "teams",
+            "defaultValue": "[",
+            "description": "<p>Team objects</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "friends",
+            "defaultValue": "[",
+            "description": "<p>Uset objects</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "staff",
+            "description": "<p>Is user a staff</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "manager",
+            "description": "<p>Is user a manager</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "admin",
+            "description": "<p>Is user a admin</p>"
+          }
+        ]
+      }
+    }
   }
 ] });
