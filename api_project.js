06bde708632a71160dd323fe7bed1273eb206894
yuvapoojary <yuvapoojary@users.noreply.github.com>
2021-04-11 12:11:52 +0000

    Site build: dbdb3d216a10ff4346f3fa2147139160ac5f1037

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-04-11T12:01:12.162Z",
+    "time": "2021-04-11T12:11:50.058Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
